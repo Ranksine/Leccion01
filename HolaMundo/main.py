5694d7fca4d03b31b951fb6c75fe9715e6f117b0
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-03-16 22:53:57 -0600

    Initial commit

---
+++ HolaMundo/main.py
@@ -0,0 +1,101 @@
+# Los comentarios se hacen con el simbolo de gato -> #
+
+#  un saludo a la consola utilizando Python (leccion 1)
+# print("Este es mi saludo desde Python...")
+
+# Lección 2: VARIABLES
+# miVariable = "Hola desde Python"
+# print(miVariable)
+# print(miVariable)
+
+# miVariable = 2
+# print(miVariable)
+
+# x = 10
+# y = 2
+# z = x + y
+# print(z)
+# print (id(x))
+
+# Leccion 3: Declaración de variables
+# nombre = "Juan Perez"
+# telefono = 3421034117
+# email = "jperez@gmail.com"
+# print(nombre)
+# print(telefono)
+# print(email)
+
+# Tipo int
+# x = 10
+# print(x)
+# print(type(x))
+
+# Tipo float
+# x = 3.1416
+# print(type(x))
+
+# Tipo String
+# x = 'Holis'
+# print(type(x))
+
+# Tipo boolean
+# x = True
+# x = False
+# print(type(x))
+
+# Lección 4: Manejo de String
+# Cadena (String)
+# miGrupoFav = 'Black Sabbath'
+# comentario = 'la mejor bandita'
+# print("Mi grupo favorito es: " + miGrupoFav)
+# print('Mi grupo favorito es:', miGrupoFav, comentario)
+
+# numero1 = "1"
+# numero2 = "2"
+# print("Concatenacion:", numero1 + numero2)
+
+# numero1 = 1
+# numero2 = 2
+# print("Suma de numeros enteros:", numero1 + numero2)
+
+# numero1 = "1"
+# numero2 = "2"
+# print("Conversion de strings a numeros enteros:", int(numero1) + int(numero2))
+
+# Tipos bool (boolean)
+# miVariable = False
+# print(miVariable)
+
+# miVariable = 1<2
+# print(miVariable)
+
+# if miVariable:
+#     print('El resultado fue verdadero')
+# else:
+#     print('El resultado fue falso')
+
+# Funcion input para procesar la entrada del usuario
+# resultado = input("Escribe un mensaje papi: ")
+# print("Valor proporcionado:",resultado)
+# print("Fin del programa")
+
+# numero1 = int(input("Escribe el primer número: "))
+# numero2 = int(input("Escribe el segundo número: "))
+# resultado = numero1 + numero2
+# print("El resultado de la suma es:",resultado)
+
+# Ejercicio Califica tu dia del 1 al 10
+# calificacion = int(input("¿Cómo estuvo tu día? (1-10): "))
+# print('Mi día estuvo de:', calificacion)
+# print(type(calificacion))
+
+# Ejercicio Se solicita incluir la siguiente información acerca de un libro:
+# titulo
+# autor
+# Debes imprimir la información en el siguiente formato:
+# Proporciona el título:
+# Proporciona el autor:
+# <titulo> fue escrito por <autor>
+titulo = input("Proporciona el titulo del libro: ")
+autor = input("Porporciona el autor del libro: ")
+print("El libro ", titulo, "fue escrito por", autor)
